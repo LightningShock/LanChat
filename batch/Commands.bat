f211ae0bd834f828a04c08de3f500791dda5650b
Youtubrer <youtubrer@user.cf>
2015-07-23 17:41:54 -0700

    NewFeatures!
    +Added Login Feature!
    +Added Register Ability!
    +Added Password Encryption!
    -Removed cmd.cmd (Random Junk)
    /Fixed Bug with commands.bat
    - [ ] Still working out some bugs.

--- batch/Commands.bat
+++ batch/Commands.bat
@@ -4,13 +4,13 @@ set /p Who=<Who
 set /p SaidWhat=<JustSaid
 set UnCaps=%SaidWhat%
 for %%i in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do call set SaidWhat=%%SaidWhat:%%i=%%i%%
-Set Command==%SaidWhat%
+Set Command=%SaidWhat%
 
      IF "%Command:~0,1%"=="!" (
-        Echo COMMAND >JustSaid
-        IF %Command:~0,4%==!SAY (
+        Echo Command >JustSaid
+        IF "%Command:~0,4%"=="!SAY" (
            echo call ncol "CONSOLE" 52 >>Lobby.bat
-           echo echo %UnCaps:~4% >>Lobby.bat
+           echo echo :%UnCaps:~5% >>Lobby.bat
            )
      IF "%Command:~0,6%"=="!CLEAR" (
         echo call ncol "%Who%" 09 >Lobby.bat
@@ -18,14 +18,14 @@ Set Command==%SaidWhat%
      )
      IF "%Command:~0,11%"=="!CHECKSTATS" (
         :Tmr
-        timeout 5
+        timeout 5 
         if exist rst (
+             echo set check==1 >>onlinelist.bat
              del rst
+             timeout 15
+             goto Tmr
             )
-        echo set check==1 >OnlineList.bat
-        timeout 15
-        if exist rst goto tmr
-        echo set check==0 >OnlineList.bat 
-     )
+         echo set check==0 >>onlinelist.bat
+        )
      )
 GOTO top
